c6c2c6b6a8172e7d9c3a17bf2bf7fe8a5b87074c
Armann David Sigurdsson <armannds@gmail.com>
2018-04-15 21:43:29 +0200

    Add Bulma

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import './index.css';
 import App from './App';
 import Store from './store';
 
-const StoreInstance = Store();
+const store = Store();
 
 ReactDOM.render(
-  <Provider store={StoreInstance}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root'),
